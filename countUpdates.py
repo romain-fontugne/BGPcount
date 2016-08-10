f135caffc60a9915ae0a1296e1397109eed7274d
romain.fontugne <romain.fontugne@gmail.com>
2016-08-10 18:02:38 +0900

    added plot and prefix filter

--- countUpdates.py
+++ countUpdates.py
@@ -3,37 +3,56 @@ from subprocess import Popen, PIPE
 import glob
 import radix
 import cPickle as pickle
+import pandas as pd
+from matplotlib import pylab as plt
 
-def readupdates(files, rtree=radix.Radix()):
+def readupdates(files, rtree=radix.Radix(), prefix=None):
 
     p0 = Popen(["bzcat"]+files, stdout=PIPE, bufsize=-1)
     p1 = Popen(["bgpdump", "-m", "-v", "-"], stdin=p0.stdout, stdout=PIPE, bufsize=-1)
 
-    counter = {"W": 0, "A":0}
+    counter = {"W": [], "A":[], "ts":[]}
     for line in p1.stdout:
       
         res = line.split('|',15)
-        counter[res[2]] += 1
         if (res[2] == "W"): 
-            continue
+            # For the plot
+            if prefix is None :
+                counter["W"].append(1)
+                counter["A"].append(0)
+                counter["ts"].append(zDt)
+            elif zPfx == "prefix":
+                counter["W"].append(1)
+                counter["A"].append(0)
+                counter["ts"].append(zDt)
         
         elif (res[2] == "A"):
             zTd, zDt, zS, zOrig, zAS, zPfx, sPath, zPro, zOr, z0, z1, z2, z3, z4, z5 = res
 
-        if rtree.search_exact(zPfx) is None:
-            node = rtree.add(zPfx)
-            node.data["firsttime"] = zDt
-            node.data["lasttime"] = zDt
-            node.data["nbUpdate"] = 1
-            node.data["path"] = sPath 
+            if rtree.search_exact(zPfx) is None:
+                node = rtree.add(zPfx)
+                node.data["firsttime"] = zDt
+                node.data["lasttime"] = zDt
+                node.data["nbUpdate"] = 1
+                node.data["path"] = sPath 
 
-        else :
-            node = rtree.search_exact(zPfx)
-            node.data["lasttime"] = zDt
-            node.data["path"] = sPath 
-            node.data["nbUpdate"] += 1
+            else :
+                node = rtree.search_exact(zPfx)
+                node.data["lasttime"] = zDt
+                node.data["path"] = sPath 
+                node.data["nbUpdate"] += 1
+            
+            # For the plot
+            if prefix is None :
+                counter["A"].append(1)
+                counter["W"].append(0)
+                counter["ts"].append(zDt)
+            elif zPfx == prefix:
+                counter["A"].append(1)
+                counter["W"].append(0)
+                counter["ts"].append(zDt)
 
-    return rtree, counter
+    return rtree, pd.DataFrame(counter)
 
 if __name__ == "__main__":
 
@@ -50,6 +69,17 @@ if __name__ == "__main__":
     files.sort()
 
     rtree, counter = readupdates(files)
+    print "%s Announces, %s Withdraws, %s unique prefixes" % (counter["A"].sum(), counter["W"].sum(), len(rtree.nodes())) 
 
-    print "%s Announces, %s Withdraws, %s unique prefixes" % (counter["A"], counter["W"], len(rtree.nodes())) 
+    counter.index = pd.to_datetime(counter["ts"], unit="s")
+    counter.drop("ts", 1)
+    counter1h = counter.resample("1H").sum()
+
+    plt.figure()
+    plt.plot(counter1h.index, counter1h["A"], label="updates")
+    # plt.plot(counter1h.index, counter1h["W"], label="withdrawals")
+    plt.ylabel("#Updates per hour")
+    plt.xticks(rotation=70)
+    plt.tight_layout()
+    plt.savefig("nbUpdates.eps")
     
